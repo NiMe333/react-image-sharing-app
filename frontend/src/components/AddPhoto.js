2b8754ae697274c601d079069ed77346b002f441
Nik <mejaknik@gmail.com>
2026-05-01 16:31:02 +0200

    refactor: changed port to 5001

--- frontend/src/components/AddPhoto.js
+++ frontend/src/components/AddPhoto.js
@@ -1,44 +1,64 @@
-import { useContext, useState } from 'react'
-import { Navigate } from 'react-router';
-import { UserContext } from '../userContext';
+import { useContext, useState } from "react";
+import { Navigate } from "react-router";
+import { UserContext } from "../userContext";
 
 function AddPhoto(props) {
-    const userContext = useContext(UserContext); 
-    const[name, setName] = useState('');
-    const[file, setFile] = useState('');
-    const[uploaded, setUploaded] = useState(false);
+  const userContext = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [file, setFile] = useState("");
+  const [uploaded, setUploaded] = useState(false);
 
-    async function onSubmit(e){
-        e.preventDefault();
+  async function onSubmit(e) {
+    e.preventDefault();
 
-        if(!name){
-            alert("Vnesite ime!");
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('name', name);
-        formData.append('image', file);
-        const res = await fetch('http://localhost:3001/photos', {
-            method: 'POST',
-            credentials: 'include',
-            body: formData
-        });
-        const data = await res.json();
-
-        setUploaded(true);
+    if (!name) {
+      alert("Vnesite ime!");
+      return;
     }
 
-    return (
-        <form className="form-group" onSubmit={onSubmit}>
-            {!userContext.user ? <Navigate replace to="/login" /> : ""}
-            {uploaded ? <Navigate replace to="/" /> : ""}
-            <input type="text" className="form-control" name="ime" placeholder="Ime slike" value={name} onChange={(e)=>{setName(e.target.value)}}/>
-            <label>Izberi sliko</label>
-            <input type="file" id="file" onChange={(e)=>{setFile(e.target.files[0])}}/>
-            <input className="btn btn-primary" type="submit" name="submit" value="Naloži" />
-        </form>
-    )
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("image", file);
+    const res = await fetch("http://localhost:5001/photos", {
+      method: "POST",
+      credentials: "include",
+      body: formData,
+    });
+    const data = await res.json();
+
+    setUploaded(true);
+  }
+
+  return (
+    <form className="form-group" onSubmit={onSubmit}>
+      {!userContext.user ? <Navigate replace to="/login" /> : ""}
+      {uploaded ? <Navigate replace to="/" /> : ""}
+      <input
+        type="text"
+        className="form-control"
+        name="ime"
+        placeholder="Ime slike"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <label>Izberi sliko</label>
+      <input
+        type="file"
+        id="file"
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }}
+      />
+      <input
+        className="btn btn-primary"
+        type="submit"
+        name="submit"
+        value="Naloži"
+      />
+    </form>
+  );
 }
 
 export default AddPhoto;
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,28 +1,30 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../userContext';
-import { Navigate } from 'react-router-dom';
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../userContext";
+import { Navigate } from "react-router-dom";
 
-function Profile(){
-    const userContext = useContext(UserContext); 
-    const [profile, setProfile] = useState({});
+function Profile() {
+  const userContext = useContext(UserContext);
+  const [profile, setProfile] = useState({});
 
-    useEffect(function(){
-        const getProfile = async function(){
-            const res = await fetch("http://localhost:3001/users/profile", {credentials: "include"});
-            const data = await res.json();
-            setProfile(data);
-        }
-        getProfile();
-    }, []);
+  useEffect(function () {
+    const getProfile = async function () {
+      const res = await fetch("http://localhost:5001/users/profile", {
+        credentials: "include",
+      });
+      const data = await res.json();
+      setProfile(data);
+    };
+    getProfile();
+  }, []);
 
-    return (
-        <>
-            {!userContext.user ? <Navigate replace to="/login" /> : ""}
-            <h1>User profile</h1>
-            <p>Username: {profile.username}</p>
-            <p>Email: {profile.email}</p>
-        </>
-    );
+  return (
+    <>
+      {!userContext.user ? <Navigate replace to="/login" /> : ""}
+      <h1>User profile</h1>
+      <p>Username: {profile.username}</p>
+      <p>Email: {profile.email}</p>
+    </>
+  );
 }
 
 export default Profile;
